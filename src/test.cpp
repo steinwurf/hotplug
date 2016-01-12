c430714fa7df3fcf0b9e612965167cc2ace28327
Lars Nielsen <lnc13.lars@gmail.com>
2016-01-12 22:50:27 +0100

    check if adding or removing usb

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,7 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include <iostream>
-
+#include <string>
 #include "hotplug.hpp"
 
 boost::asio::io_service io_service;
@@ -16,11 +16,18 @@ void read_handle(boost::asio::posix::stream_descriptor* descriptor,
         int file_descriptor = descriptor.native_handle();
         if(udev_monitor_get_fd(hotplug::hotplug_monitor) == file_descriptor)
         {
-            udev_device* dev = udev_monitor_receive_device(hotplug_monitor);
+            udev_device* dev = udev_monitor_receive_device(hotplug::hotplug_monitor);
             if(udev_device_get_action(dev) != NULL &&
                udev_device_get_devnode(dev) != NULL)
             {
-                //print result
+
+                if(udev_device_get_action(dev).compare("add") == 0)
+                {
+                    // call add procedure with devnode
+                } else if(udev_device_get_action(dev).compare("remove") == 0)
+                {
+                    // call remove procedure with devnode
+                }
             }
             udev_device_unref(dev);
         }
