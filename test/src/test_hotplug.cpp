76c03310d5840712b1feb4d824dc54cde9cbbb76
Lars Nielsen <lnc13.lars@gmail.com>
2016-01-29 14:16:18 +0100

    added test

--- test/src/test_hotplug.cpp
+++ test/src/test_hotplug.cpp
@@ -1,4 +1,3 @@
-#include "hotplug.hpp"
 // Copyright Steinwurf ApS 2016.
 // Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
 // See accompanying file LICENSE.rst or
@@ -12,6 +11,8 @@
 #include <functional>
 #include <gtest/gtest.h>
 
+#include "../helper_functions.hpp"
+
 
 
 
@@ -21,12 +22,15 @@ namespace hotplug
     static void test_hotplug()
     {
         std::function<void(std::string)> add_callback = [](std::string add_string){
+
+            EXPECT_TRUE(is_correct_action("add", get_action_string(add_string)));
             std::cout << "add action: "  << add_string << std::endl;
-        }
+        };
 
         std::function<void(std::string)> remove_callback = [](std::string remove_string){
+            EXPECT_TRUE(is_correct_action("remove", get_action_string(remove_string)));
             std::cout << "remove action: "  << remove_string << std::endl;
-        }
+        };
 
         start_hotplug(add_callback, remove_callback);
     }
